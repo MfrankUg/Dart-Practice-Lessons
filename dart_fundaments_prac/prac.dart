1104f0d0c6ac3158f69bb86324295e503c97e86a
MfrankUg <muhindofrank2001@gmail.com>
2025-12-21 12:16:21 +0300

    find word in a string practice

---
+++ dart_fundaments_prac/prac.dart
@@ -0,0 +1,8 @@
+void main() {
+  var findList = "This is FRANK trying out this new tech";
+  if (findList.contains("FRANK")) {
+    print("the word contains the name FRANK");
+  } else {
+    print("FRANK word not found");
+  }
+}
