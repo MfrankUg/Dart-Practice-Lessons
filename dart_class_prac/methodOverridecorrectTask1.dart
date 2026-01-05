e36d3283a5ba3e9d048d3e61a381832a05004b29
MfrankUg <muhindofrank2001@gmail.com>
2026-01-05 22:31:20 +0300

    changes

--- dart_class_prac/methodOverridecorrectTask1.dart
+++ dart_class_prac/methodOverridecorrectTask1.dart
@@ -1,6 +1,14 @@
 // Correction of Task1
 class Shape {
-  double calculateArea() => 0; 
-  
+  double calculateArea() => 0;
 }
+
+class Rectangle extends Shape {
+  double height;
+  double width;
+  Rectangle(this.height, this.width);
+  @override
+  double calculateArea() => height * width;
+}
+
 
