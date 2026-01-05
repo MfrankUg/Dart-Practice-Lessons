a701c2d1dffe5b8bee4bfc68fa788e3882c6cea0
MfrankUg <muhindofrank2001@gmail.com>
2026-01-05 22:34:16 +0300

    changes on overriding

--- dart_class_prac/methodOverridecorrectTask1.dart
+++ dart_class_prac/methodOverridecorrectTask1.dart
@@ -11,4 +11,10 @@ class Rectangle extends Shape {
   double calculateArea() => height * width;
 }
 
+class Circle extends Shape {
+  double radius;
+  Circle(this.radius);
+  @override
+  double calculateArea() => 3.14 * radius * radius;
+}
 
