c11f1c6b90ae92dad8e9a5280520f7af88473bb5
MfrankUg <muhindofrank2001@gmail.com>
2026-01-21 21:29:09 +0300

    runes in dart

---
+++ others/prac.dart
@@ -0,0 +1,12 @@
+// In Dart, declaring variables as Runes are the UTF-32 code points of a String. 
+void main() {
+// Emoji smiling angel Unicode is u+1f607
+// Remove the Plus sign and replace with curly brackets
+Runes myEmoji = Runes('\u{1f607}');
+print(myEmoji);
+// Result from print statement
+// (128519)
+print(String.fromCharCodes(myEmoji));
+// Result from print statement
+// 😇
+}
