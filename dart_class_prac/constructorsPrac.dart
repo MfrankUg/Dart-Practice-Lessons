fcd6ab35f6cd6602d31618dbe608ca0d1617bcd4
MfrankUg <muhindofrank2001@gmail.com>
2026-02-01 21:28:25 +0300

    changes

--- dart_class_prac/constructorsPrac.dart
+++ dart_class_prac/constructorsPrac.dart
@@ -1,1 +1,8 @@
-// A construct
+// A constructors are special functions that run automatically once an instance of a class is created
+// example
+
+class Rectangle {
+  double height;
+  double width;
+  Rectangle(this.height, this.width);
+}
