695281154d464fb330a5a7c80b3482dbf325c4e8
MfrankUg <muhindofrank2001@gmail.com>
2026-02-06 13:59:32 +0300

    string Interpolation practice

---
+++ others/stringInterpo.dart
@@ -0,0 +1,7 @@
+void main() {
+  String firstName = "Frank";
+  String secondName = "Muhindo";
+  //String fullName = firstName + secondName;
+  String fullName = "$firstName $secondName";
+  print(fullName);
+}
