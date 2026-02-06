4cb3853bc3d936a1c8dd110da84faf10b2cc53f0
MfrankUg <muhindofrank2001@gmail.com>
2026-02-06 14:41:13 +0300

    more

--- others/StringInterpo1.dart
+++ others/StringInterpo1.dart
@@ -3,4 +3,8 @@ void main() {
   int oranges = 5;
   String message = "$name has $oranges in his bag";
   print(message);
+  int a = 26;
+  int b = 40;
+  String result = "the sum of $a + $b = ${a + b}";
+  print(result);
 }
